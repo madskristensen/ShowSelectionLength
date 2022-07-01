2f097707151bd08fc479fb0a2341c4ce1aa3d819
alstrokau <strokov.aleksandr@gmail.com>
2022-07-02 01:09:36 +0300

    Add lines count

--- src/CommandHandler.cs
+++ src/CommandHandler.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.ComponentModel.Composition;
-using Community.VisualStudio.Toolkit;
+﻿using Community.VisualStudio.Toolkit;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
+using System;
+using System.ComponentModel.Composition;
+using System.Linq;
 
 namespace ShowSelectionLength
 {
@@ -37,15 +38,17 @@ namespace ShowSelectionLength
                 }
 
                 var length = 0;
+                var linesCount = 0;
 
                 foreach (SnapshotSpan snapshotSpan in selection.SelectedSpans)
                 {
                     length += snapshotSpan.Length;
+                    linesCount += snapshotSpan.GetText().Count(c => c == '\n') + 1;
                 }
 
                 if (length > 0)
                 {
-                    await VS.StatusBar.ShowMessageAsync($"Selection {length}");
+                    await VS.StatusBar.ShowMessageAsync($"Selection {length}C {linesCount}L");
                 }
 
             }).FireAndForget();
